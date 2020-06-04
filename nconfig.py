f67922c39a6824bb6dd59ad095b1684bfe063e33
Noah Jutz <noah.jutz@gmail.com>
2020-06-04 17:32:42 +0200

    Edit prompt style

--- nconfig.py
+++ nconfig.py
@@ -56,27 +56,34 @@ def cli():
 @cli.command()
 def auto_install():
     """Configure everything according to passed options"""
+
+    """ prompts """
     # Initial prompts
-    restore_dotfiles = click.confirm("Restore dotfiles?")
-    restore_backup = click.confirm("Restore backup?")
-    install_packages = click.confirm("Install packages?")
+    restore_dotfiles = click.confirm(click.style("> Restore dotfiles?", fg="magenta"))
+    restore_backup = click.confirm(click.style("> Restore backup?", fg="magenta"))
+    install_packages = click.confirm(click.style("> Install packages?", fg="magenta"))
 
     # Package specific prompts
     if install_packages:
         package_manager = \
-            click.prompt("  Package manager", type=click.Choice(choices=packages.keys(), case_sensitive=False))
+            click.prompt("  > Package manager", type=click.Choice(choices=packages.keys(), case_sensitive=False))
         package_manager = pacman if package_manager == "PACMAN" else deb
 
         # Prompt each package group
         groups = {}
         packages_to_install = list()
         for group in package_manager:
-            groups[group] = click.confirm("    Install {} packages?".format(click.style(group, fg="blue")))
+            groups[group] = click.confirm("    > Install {} packages?".format(click.style(group, fg="blue")))
             if groups[group]:
                 for package in package_manager[group]:
-                    if click.confirm("      Install {}?".format(click.style(package, fg="green"))):
+                    if click.confirm("      > Install {}?".format(click.style(package, fg="green"))):
                         packages_to_install.append(package)
-        click.echo("Packages to install: {}".format(str(packages_to_install)))
+        click.echo()
+        click.secho("Packages to install:", fg="magenta")
+        for package in packages_to_install:
+            click.echo("  - {}".format(package))
+
+    """ Install """
 
 
 @cli.command()
